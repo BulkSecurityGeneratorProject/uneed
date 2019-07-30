b974dddd383b620830c17a009a53553aca3f11e1
volcanohong <volcanohong@gmail.com>
2019-07-29 22:12:34 -0700

    Dashboard - service - get userJob by current user

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserInfo } from 'app/shared/model/user-info.model';
+import { IUserJob } from 'app/shared/model/user-job.model';
+import { DashboardService } from './dashboard.service';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { JhiAlertService } from 'ng-jhipster';
 
 @Component({
   selector: 'jhi-dashboard',
@@ -6,7 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  userInfo: IUserInfo;
 
-  ngOnInit() {}
+  userJob: IUserJob;
+
+  constructor(protected dashboardService: DashboardService, protected jhiAlertService: JhiAlertService) {}
+
+  ngOnInit() {
+    this.dashboardService
+      .currentJob()
+      .subscribe((res: HttpResponse<IUserJob[]>) => this.currentJob(res.body), (res: HttpErrorResponse) => this.onError(res.message));
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
+  }
+
+  private currentJob(data: IUserJob[]) {
+    if (data.length > 0) {
+      this.userJob = data[0];
+      console.log(this.userJob);
+    }
+  }
 }
--- src/main/webapp/app/dashboard/dashboard.module.ts
+++ src/main/webapp/app/dashboard/dashboard.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { UneedSharedModule } from 'app/shared';
 import { RouterModule } from '@angular/router';
-import { DashboardRoute } from 'app/dashboard/dashboard.route';
+import { DashboardRoute } from './dashboard.route';
+import { DashboardService } from './dashboard.service';
 
 @NgModule({
   declarations: [DashboardComponent],
+  providers: [DashboardService],
   imports: [UneedSharedModule, RouterModule.forChild(DashboardRoute)]
 })
 export class DashboardModule {}
