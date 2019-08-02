14885cc68fa9c92bfb56afd1e9de421e438436db
volcanohong <volcanohong@gmail.com>
2019-08-01 19:48:42 -0700

    My Service - combine w. settings

--- src/main/webapp/app/user/acc-info/acc-info.component.ts
+++ src/main/webapp/app/user/acc-info/acc-info.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IUserInfo, UserInfo } from 'app/shared/model/user-info.model';
-import { JhiAlertService } from 'ng-jhipster';
+import { JhiAlertService, JhiLanguageService } from 'ng-jhipster';
 import { FormBuilder, Validators } from '@angular/forms';
 import { filter, map } from 'rxjs/operators';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AccInfoService } from './acc-info.service';
 import { ActivatedRoute } from '@angular/router';
-import { User } from 'app/core';
+import { AccountService, JhiLanguageHelper, User } from 'app/core';
 
 @Component({
   selector: 'jhi-myaccount',
@@ -16,7 +16,8 @@ import { User } from 'app/core';
 })
 export class AccInfoComponent implements OnInit {
   isSaving: boolean;
-
+  error: string;
+  success: string;
   birthDateDp: any;
   user: User;
   editForm = this.fb.group({
@@ -30,17 +31,37 @@ export class AccInfoComponent implements OnInit {
     user: [Validators.required]
   });
 
+  languages: any[];
+  settingsForm = this.fb.group({
+    firstName: [undefined, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
+    lastName: [undefined, [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
+    email: [undefined, [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email]],
+    activated: [false],
+    authorities: [[]],
+    langKey: ['en'],
+    login: [],
+    imageUrl: []
+  });
+
   constructor(
-    protected jhiAlertService: JhiAlertService,
-    protected accInfoService: AccInfoService,
+    private accountService: AccountService,
+    private accInfoService: AccInfoService,
     private route: ActivatedRoute,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private jhiAlertService: JhiAlertService,
+    private languageService: JhiLanguageService,
+    private languageHelper: JhiLanguageHelper
   ) {}
 
   ngOnInit() {
     this.isSaving = false;
     this.route.data.subscribe(({ user }) => (this.user = user.body ? user.body : user));
-
+    this.accountService.identity().then(account => {
+      this.updateAccount(account);
+    });
+    this.languageHelper.getAll().then(languages => {
+      this.languages = languages;
+    });
     this.accInfoService
       .current()
       .pipe(
@@ -70,6 +91,7 @@ export class AccInfoComponent implements OnInit {
   }
 
   save() {
+    this.saveAccount();
     this.isSaving = true;
     const userInfo = this.createFromForm();
     if (userInfo.id !== undefined) {
@@ -107,5 +129,55 @@ export class AccInfoComponent implements OnInit {
   }
   protected onError(errorMessage: string) {
     this.jhiAlertService.error(errorMessage, null, null);
+  }
+
+  saveAccount() {
+    const settingsAccount = this.accountFromForm();
+    this.accountService.save(settingsAccount).subscribe(
+      () => {
+        this.error = null;
+        this.success = 'OK';
+        this.accountService.identity(true).then(account => {
+          this.updateAccount(account);
+        });
+        this.languageService.getCurrent().then(current => {
+          if (settingsAccount.langKey !== current) {
+            this.languageService.changeLanguage(settingsAccount.langKey);
+          }
+        });
+      },
+      () => {
+        this.success = null;
+        this.error = 'ERROR';
+      }
+    );
+  }
+
+  private accountFromForm(): any {
+    const account = {};
+    return {
+      ...account,
+      firstName: this.settingsForm.get('firstName').value,
+      lastName: this.settingsForm.get('lastName').value,
+      email: this.settingsForm.get('email').value,
+      activated: this.settingsForm.get('activated').value,
+      authorities: this.settingsForm.get('authorities').value,
+      langKey: this.settingsForm.get('langKey').value,
+      login: this.settingsForm.get('login').value,
+      imageUrl: this.settingsForm.get('imageUrl').value
+    };
+  }
+
+  updateAccount(account: any): void {
+    this.settingsForm.patchValue({
+      firstName: account.firstName,
+      lastName: account.lastName,
+      email: account.email,
+      activated: account.activated,
+      authorities: account.authorities,
+      langKey: account.langKey,
+      login: account.login,
+      imageUrl: account.imageUrl
+    });
   }
 }
